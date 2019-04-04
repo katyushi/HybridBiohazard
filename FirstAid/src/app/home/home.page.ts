d47e7e0c926340e682665ee1f2db9aafa7144e48
hugo lazzari <hugo.luiz@outlook.com>
2019-04-04 14:55:24 -0300

    begin of some plugin tests
    testing globalization plugin to do some translations

--- FirstAid/src/app/home/home.page.ts
+++ FirstAid/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Globalization } from '@ionic-native/globalization/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private globalization: Globalization) { }
+  public getPrefLang() {
+    this.globalization.getPreferredLanguage()
+  .then(res => console.log(res))
+  .catch(e => console.log(e));
+
+  }
 
 }
