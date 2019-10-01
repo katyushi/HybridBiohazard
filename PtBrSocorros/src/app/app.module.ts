43753536a6a4d647fc68fb55179e0385ad563acf
hugo lazzari <hugo.luiz@outlook.com>
2019-09-30 21:41:48 -0300

    minor changes

--- PtBrSocorros/src/app/app.module.ts
+++ PtBrSocorros/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppVersion } from '@ionic-native/app-version/ngx';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+	AppVersion,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- PtBrSocorros/src/app/sobre/sobre.page.ts
+++ PtBrSocorros/src/app/sobre/sobre.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppVersion } from '@ionic-native/app-version/ngx';
 
 @Component({
   selector: 'app-sobre',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobrePage implements OnInit {
 
-  constructor() { }
+ AppName:string;
+ PackageName:string;
+ VersionCode:string|number;
+ VersionNumber:string;
+
+ constructor(private appVersion: AppVersion) {
+   this.appVersion.getAppName().then(value => {
+     this.AppName = value;
+   }).catch(err => {
+     alert(err);
+   });
+   this.appVersion.getPackageName().then(value => {
+     this.PackageName = value;
+   }).catch(err => {
+     alert(err);
+   });
+   this.appVersion.getVersionCode().then(value => {
+     this.VersionCode = value;
+   }).catch(err => {
+     alert(err);
+   });
+   this.appVersion.getVersionNumber().then(value => {
+     this.VersionNumber = value;
+   }).catch(err => {
+     alert(err);
+   });
+  }
 
   ngOnInit() {
   }
